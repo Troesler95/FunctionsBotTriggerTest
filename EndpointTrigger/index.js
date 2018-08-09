e4806262dbca73de1a7565fbb1fb37b61a2c0c93
TRoesler <troesler95@gmail.com>
2018-08-09 14:34:45 -0500

    change botid in http function

--- EndpointTrigger/index.js
+++ EndpointTrigger/index.js
@@ -2,7 +2,7 @@ module.exports = function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
     context.log(req);
-    if (req.query)
+    if (req.query.text != "undefined")
         var message = {
             'text': req.query.text,
             'address': req.query.address
