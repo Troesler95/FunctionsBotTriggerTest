74219b8c9372d6a0a9913b9ab2e5d56e56d794d0
TRoesler <troesler95@gmail.com>
2018-08-09 15:09:25 -0500

    fixed trigger fuction to look in body, not query

--- EndpointTrigger/index.js
+++ EndpointTrigger/index.js
@@ -2,16 +2,10 @@ module.exports = function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
     context.log(req);
-    if (req.query.text != "undefined")
-        var message = {
-            'text': req.query.text,
-            'address': req.query.address
-        }
-    else
-        var message = {
-            'text': 'test',
-            'address': 'none'
-        }
+    var message = {
+        'text': req.body.text,
+        'address': req.body.address
+    }
 
     context.log('Message: \n{\n\t\'text\': \'' + message.text + '\',\n\t\'address\': \''+message.address+'\'\n}');
     context.done(null, message);
